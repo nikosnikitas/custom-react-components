e84a1d80a211e291520fcfbc75895caf4e14d3ae
nikosnikitas <64796676+nikosnikitas@users.noreply.github.com>
2022-03-15 17:58:12 +0200

    Create RandomPosts.jsx
    Random Posts component.

---
+++ RandomPosts.jsx
@@ -0,0 +1,59 @@
+/************************************************************* 
+* Random posts with dummy data that will appear on the feed. *
+* Author: Nikos-Nikitas                                      *
+**************************************************************/
+
+class RandomPosts extends React.Component {
+      constructor(props) {
+          super(props);
+          this.state = {
+              randomtxt : [
+              'Alice posted: What a wonderful day!',
+              'Bob posted: So excited to see the match!',
+              'Charlie posted: Commodo varius. Curabitur condimentum etiam viverra felis felis.',
+              'David posted: Hendrerit suscipit lacinia. Dui. Primis vestibulum rhoncus taciti porta interdum lacinia vivamus vitae justo sollicitudin.',
+              'Eric posted: Viverra ante dui. Fusce nonummy mattis senectus inceptos interdum. Torquent aptent non praesent morbi ipsum tristique. Tristique. Magnis integer.'
+              ],
+              r:''
+          };
+
+          this.nextR = this.nextR.bind(this);
+      }
+          //random posts are generated every 3 seconds by rID.
+
+      componentDidMount() {
+      this.rID = setInterval(
+          () => this.nextR(),
+          3000
+      );   
+  }
+
+  componentWillUnmount() {
+      clearInterval(this.rID);
+  }
+  nextR() {
+      this.setState(
+          {
+              r: this.state.randomtxt[Math.ceil(Math.random() * 4)]
+          }
+      );
+  }
+
+      render() {
+
+          return(
+              <div id="randomdiv">
+                  <h3> What others post.</h3>
+                  <br/>
+                  <ul id="randomlist">
+                      <li id="randompost" >
+                      {this.state.r}
+                      </li>
+                      <br/>
+                  </ul>
+              </div>
+          );
+
+      }
+
+  }
