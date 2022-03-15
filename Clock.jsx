1147efa24293dcecf7e0c50b80c025efec7ddabd
nikosnikitas <64796676+nikosnikitas@users.noreply.github.com>
2022-03-15 17:50:04 +0200

    Create Clock.jsx
    Digital Clock component.

---
+++ Clock.jsx
@@ -0,0 +1,43 @@
+/*****************************
+* A digital Clock Component. *
+* Author: Nikos-Nikitas      *
+******************************/
+
+class Clock extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+                date: new Date()
+            };
+    }
+
+    //if the component mounts it starts the clock ticking, if it unmounts the ticking stops.
+    componentDidMount() {
+        this.tID = setInterval(
+            () => this.moveTime(),
+            1000
+        );   
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.tID);
+    }
+    moveTime() {
+        this.setState(
+            {
+                date: new Date()
+            }
+        );
+    }
+    
+    //styling using className for css class.
+
+        render() {
+            return (
+            <div className="the_clock"> 
+                <h2 className="the_clock"> <b> {this.state.date.toLocaleTimeString()} </b></h2>    
+            </div>
+            );
+        }
+    }
